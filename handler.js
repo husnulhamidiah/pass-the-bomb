c68c73cdd8026934184d3d2bac801768ae831ffb
Avreghly Barra <avreghly@gmail.com>
2020-12-23 13:53:22 +0700

    add todos

--- handler.js
+++ handler.js
@@ -1,7 +1,34 @@
+import store from './store'
+
 export default {
-  handleCreateGame: (sock, data) => { return data },
-  handleJoinGame: (sock, data) => { return data },
-  handleStartGame: (sock, data) => { return data },
-  handleTapBomb: (sock, data) => { return data },
-  handlePassBomb: (sock, data) => { return data }
+  handleCreateGame: (sock, data) => {
+    const id = '1234' // TODO: generate unique easy id
+    store.setRoom(id, {
+      id: id,
+      players: [],
+      rounds: []
+    })
+  },
+
+  handleJoinGame: (sock, data) => {
+    // TODO: update room, add player id and socket reference to room
+  },
+
+  handleStartGame: (sock, data) => {
+    // TODO: shuffle player order
+    // TODO: generate rounds
+    // TODO: set round cursor
+    // TODO: set player cursor
+
+  },
+
+  handleTapBomb: (sock, data) => {
+    // TODO: decrement current round's hitnumber
+
+  },
+
+  handlePassBomb: (sock, data) => {
+    // TODO: set cursor to next player
+  }
+
 }
