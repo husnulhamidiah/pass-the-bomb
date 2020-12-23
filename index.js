ebbbbdd40bfc7d6fb4bb39adaf424bbc122ebe3e
Avré Barra <avreghly@gmail.com>
2020-12-23 13:19:52 +0700

    avre/add test panel (#1)

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
 import {} from 'dotenv/config'
 import http from 'http'
+import path from 'path'
+import express from 'express'
 import socket from 'socket.io'
 
-// setup http and websocket server
-const server = http.createServer()
-const io = socket(server)
+const app = express()
+const server = http.createServer(app)
 
+app.get('/', (req, res) => res.json({ msg: 'hello world' }))
+app.get('/dev/panel/', (req, res) => res.sendFile(path.join(__dirname, 'dev/testpanel.html')))
+
+// setup socket server
+const io = socket(server)
 io.on('connection', socket => {
-  socket.on('ping', () => {
-    socket.emit('ping', 'pong')
-  })
+  socket.on('ping', () => socket.emit('ping', 'pong'))
+
+  socket.emit('welcome', 'welcome')
 })
 
+// start server
 const port = process.env.PORT
-
 server.listen(port, () => {
   console.log(`server running on port ${port}`)
 })
